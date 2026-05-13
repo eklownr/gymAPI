5702cb5ec237df1dc57b070cf974891f011d595f
eklownr <eklownr@gmail.com>
2026-05-13 20:22:08 +0200

    add gym 1 - 3

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ const Home = () => {
 			<h2>Home GymAPI - Module3 Auth0 with express</h2>
 			<a href="http://localhost:5173/profile"> Profile </a>
 			<a href="http://localhost:3000/gyms"> List all gyms </a>
+			<a href="http://localhost:3000/gyms/1"> Gym nr 1 </a>
+			<a href="http://localhost:3000/gyms/2"> Gym nr 2 </a>
 			<a href="http://localhost:3000/gyms/3"> Gym nr 3 </a>
 		</div>
 	);
