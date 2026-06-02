37d5ac754c10dd3525d3ffaf0482fe82ad52d6f7
eklownr <eklownr@gmail.com>
2026-06-02 14:37:56 +0200

    deploy on render, vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+/**
+ * working for http://localhost:5173/
+ */
+// export default defineConfig({
+// 	plugins: [react()],
+// 	test: {
+// 		globals: true,
+// 		environment: "jsdom", // Enables browser-like environment
+// 	},
+// });
+
+/**
+ * Deploy on Render
+ */
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "jsdom", // Enables browser-like environment
+ 	plugins: [react()],
+	build: {
+	  outDir: 'dist',
+	  emptyOutDir: true,
 	},
-});
+	// Om du använder React, se till att basePath är korrekt
+	base: './', 
+  })   
